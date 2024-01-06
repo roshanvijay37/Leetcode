e5e7ed0bd1e85111feb7adcc6cafcf3bd478dbc2
Roshan Vijay Shetty <rshetty@csod.com>
2024-01-07 00:22:42 +0530

    Remove Duplicates From Sorted Array

--- ThreeSumArrayQuestionTwo/Solution.java
+++ ThreeSumArrayQuestionTwo/Solution.java
@@ -4,21 +4,6 @@ import java.util.List;
 
 public class ThreeSum {
     public List<List<Integer>> threeSum(int[] nums) {
-        //Question is If we get a array of numbers nums[] with some number
-        //We should return all the possible triplets in nums[] such that nums[i]+nums[j]+nums[k] = 0
-        //Example {-1, 0, 1}
-        //We start solving this by sorting the array and taking target = -nums[i]
-        //i.e if nums[i] = 5 then target is -5
-        //We initialize left = 0 and right = nums.length - 1
-        //As long as left<right we add we traverse through all the entries of the nums array
-        //If target+nums[left]+nums[right] == 0, Add (nums[i],nums[left],nums[right]) to our solutionlistOfLists
-        //If num[left] and nums[left+1] are same we make left++
-        //likewise If nums[right] and nums[right - 1] are same we make right--;
-        //This is because, We don't want duplicate entries in our solution
-        //Make left++ and right-- This is because if target+nums[left]+nums[right] == 0, We have already added it to the solutionListOfLists
-        //And we want to shift both the pointers left and right by 1 bit each
-        //if target is less than nums[left]+nums[right] We make left++
-        //Likewise if target is greater than nums[left]+nums[right] we make right--
         List<List<Integer>> solutionListOfLists = new ArrayList<>();
         Arrays.sort(nums);
         for(int i=0;i< nums.length-2;i++)
